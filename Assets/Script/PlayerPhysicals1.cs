1ec64816bf6dcf2384dc9180f9c9b724d5cc05b1
MathieuMar66 <84508890+MathieuMar66@users.noreply.github.com>
2022-05-10 14:27:46 +0200

    DoubleJump + probleme saut

--- Assets/Script/PlayerPhysicals1.cs
+++ Assets/Script/PlayerPhysicals1.cs
@@ -10,6 +10,10 @@ public class PlayerPhysicals1 : MonoBehaviour
     public float speed = 8f;
     public float gravity = -27f;
     public float jumpHeight = 3f;
+    public int maxJumpCount = 1;
+    public int jumpRemaning = 0;
+    public bool dblJump = false;
+    public bool noJump = false;
 
     public Transform groundCheck;
     public float groundDistance = 0.4f;
@@ -29,6 +33,38 @@ public class PlayerPhysicals1 : MonoBehaviour
         if(isGrounded && velocity.y < 0)
         {
             velocity.y = -2f;
+            jumpRemaning = maxJumpCount;
+        }
+        
+        if(!isGrounded && dblJump == false)
+        {
+            maxJumpCount = 0;
+            jumpRemaning = maxJumpCount;
+        }
+        else
+        {
+            if(dblJump == false)
+            {
+                maxJumpCount = 1;
+            }
+        }
+
+        if(!isGrounded && dblJump == true)
+        {
+            if(noJump == false)
+            {
+                maxJumpCount = 1;
+                jumpRemaning = maxJumpCount;
+                noJump = true;
+            }
+        }
+        else
+        {
+            if(dblJump == true)
+            {
+                maxJumpCount = 2;
+                noJump = false;
+            }
         }
 
         float x = Input.GetAxis("Horizontal");
@@ -38,9 +74,13 @@ public class PlayerPhysicals1 : MonoBehaviour
 
         controller.Move(move * speed * Time.deltaTime);
 
-        if(Input.GetButtonDown("Jump") && isGrounded)
+        if(Input.GetButtonDown("Jump"))
         {
+            if((jumpRemaning > 0))
+            {
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            jumpRemaning -= 1;
+            }
         }
 
         if(Input.GetKey(KeyCode.LeftShift))
@@ -49,7 +89,10 @@ public class PlayerPhysicals1 : MonoBehaviour
         }
         else
         {
+            if(isGrounded)
+            {
             speed = 8f;
+            }
         }
 
         if(Input.GetKey(KeyCode.LeftControl))
@@ -68,6 +111,16 @@ public class PlayerPhysicals1 : MonoBehaviour
         velocity.y += gravity * Time.deltaTime;
 
         controller.Move(velocity * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        
+        if(other.CompareTag("BonusJump"))
+        {
+            maxJumpCount = 2;
+            dblJump = true;
+        }  
+        
     }
     
     
